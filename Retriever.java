b74488884480c0564a8a81190c7998d71ac60d8a
brianmoses <bmoses00@stuy.edu>
2019-03-01 17:09:01 -0500

    Improved Retriever.java

--- Retriever.java
+++ Retriever.java
@@ -1,6 +1,6 @@
 public class Retriever extends Pet{
 	public String name;
     public String speak() {
-        return "arf";
+        return "The history of all hitherto existing society is the history of class struggles. Freemen and journeyman, in a word, oppressor and oppressed, stood in constant opposition to one another, carried on an uninterrupted, now hidden, now open fight, a fight that each time ended, either in a revolutionary reconstitution of society at large, or in the common ruin of the contending classes.";
     }
 }
